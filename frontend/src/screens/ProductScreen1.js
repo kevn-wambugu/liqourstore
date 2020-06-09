2e8877ac224fcccc78324c09cdec9b9a4a65422d
Kevin Wambugu <wambugunjoroge23@gmail.com>
2020-06-09 11:57:35 +0300

    "product screen"

--- frontend/src/screens/ProductScreen1.js
+++ frontend/src/screens/ProductScreen1.js
@@ -1,188 +1,115 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import LoadingBox from '../components/LoadingBox';
-import ErrorBox from '../components/ErrorBox';
-import { detailsProduct, saveProductReview } from '../actions/Productactions';
-import Rating from '../components/Rating';
-import { PRODUCT_REVIEW_SAVE_RESET } from '../constants/ProductConstants';
+import { useSelector, useDispatch } from 'react-redux';
+import { detailsProduct } from '../actions/Productactions';
 
 function ProductScreen(props) {
   const [qty, setQty] = useState(1);
-  const [comment, setComment] = useState(''); 
-  const [rating, setRating] = useState('');
-  const addToCart = () => {
-    props.history.push(`/cart/${props.match.params.id}?qty=${qty}`);
-  };
+  const productDetails = useSelector(state => state.productDetails);
+  const { product, loading, error } = productDetails;
   const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList);
+  const { products} = productList;
 
-
-  const productReviewSave = useSelector((state) => state.productReviewSave);
-  const { loading: loadingSaveReview, error: errorSaveReview, success: successSaveReview}  = productReviewSave;
 
   useEffect(() => {
-    if (successSaveReview) {
-      setComment('');
-      setRating('');
-      alert('Review Submitted');
-      dispatch({ type: PRODUCT_REVIEW_SAVE_RESET });
-    } else {
-      dispatch(detailsProduct(props.match.params.id));
-    }
+    dispatch(detailsProduct(props.match.params.id));
     return () => {
       //
     };
-  }, [successSaveReview]);
-  const submitHandler = (e) => {
-    e.preventDefault();
-    dispatch(saveProductReview(props.match.params.id, { comment, rating }));
-  };
-  const productDetails = useSelector((state) => state.productDetails);
-  const { loading, error, product } = productDetails;
+  }, []);
 
-  return (
-    loading ? <LoadingBox /> : error ? <ErrorBox message={error} />
-      : (
-        <div>
-          <div className="back-to-results">
-            <Link to="/"> ‹ Back to results</Link>
-          </div>
+  const handleAddToCart = () => {
+    props.history.push("/cart/" + props.match.params.id + "?qty=" + qty)
+  }
+
+  return <div>
+    <div className="back-to-result">
+      <Link to="/">Back to result</Link>
+    </div>
+    {loading ? <div>Loading...</div> :
+      error ? <div>{error} </div> :
+        (
           <div className="details">
             <div className="details-image">
-              <img src={product.image} alt="product" />
+              <img src={product.image} alt="product" ></img>
             </div>
             <div className="details-info">
               <ul>
                 <li>
-                  <h3>{product.name}</h3>
+                  <h4>{product.name}</h4>
                 </li>
                 <li>
-                  <Rating value={product.rating} />
-                  (
-                  {product.numReviews}
-                  {' '}
-                  Customer reviews
-                  )
+                <Link to={'/Category/' + product.category}>{product.category} </Link>
                 </li>
                 <li>
-                  Price:
-                  {' '}
-                  <span className="price">
-                    {' '}
-                    KSH
-                    {product.price}
-                  </span>
+                  {product.brand} 
                 </li>
-
                 <li>
-                  Description:
-                  <div>{product.description}</div>
+                   <b>Ksh {product.price}</b>
                 </li>
+                <li>
+                  <div>
+                  {product.description}
+                  </div>
+                </li>
+                <li>
+                  <div id="share-buttons">
+                    <a href="mailto:?Subject=Simple Share Buttons&amp;Body=I%20saw%20this%20and%20thought%20of%20you!%20 https://alchemisliqorstore.co.ke">
+                      <img src="https://simplesharebuttons.com/images/somacro/email.png" alt="Email" />
+                    </a>
+                    <a href="http://www.facebook.com/sharer.php?u=https://alchemisliqorstore.co.ke" target="_blank">
+                      <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" />
+                    </a>
+                    <a href="https://twitter.com/share?url=https://alchemisliqorstore.co.ke&amp;text=Simple%20Share%20Buttons&amp;hashtags=simplesharebuttons" target="_blank">
+                     <img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" />
+                    </a>
+                    <a href="https://plus.google.com/share?url=https://alchemisliqorstore.co.ke" target="_blank">
+                      <img src="https://simplesharebuttons.com/images/somacro/google.png" alt="Google" />
+                    </a>
+                    <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=https://alchemisliqorstore.co.ke" target="_blank">
+                      <img src="https://simplesharebuttons.com/images/somacro/linkedin.png" alt="LinkedIn" />
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div>{product.review}
+                    </div>                  
+                </li>
+                <li>
+                  <p className="comment-form-comment">
+                  <label  for="comment"></label>
+                  <textarea id="comment" name="comment" cols="45" rows="8" ></textarea>
+                  </p>
+                </li>
+                
               </ul>
             </div>
-            <div className="details-actions">
+            <div className="details-action">
               <ul>
                 <li>
-                  Price: KSH
-                  {product.price}
+                  Price:KSh {product.price}
                 </li>
                 <li>
-                  State:
-                  {' '}
-                  {product.countInStock > 0 ? 'InStock' : 'Unavailable'}
+                  Status: {product.countInStock > 0 ? "In Stock" : "Unavailable."}
                 </li>
-                {
-                  product.countInStock && (
-                    <li>
-                      Qty:
-                      <select value={qty} onChange={(e) => setQty(e.target.value)}>
-                        {[...Array(product.countInStock).keys()].map((x) => (
-                          <option key={x + 1} value={x + 1}>
-                            {x + 1}
-                          </option>
-                        ))}
-                      </select>
-                    </li>
-                  )
-                }
-                {
-                  product.countInStock && (
-                    <li>
-                      <button type="button" onClick={addToCart} className="button primary">
-                        Add to Cart
-                      </button>
-                    </li>
-                  )
-                }
+                <li>
+                  Quantity: <select value={qty} onChange={(e) => { setQty(e.target.value) }}>
+                    {[...Array(product.countInStock).keys()].map(x =>
+                      <option key={x + 1} value={x + 1}>{x + 1}</option>
+                    )}
+                  </select>
+                </li>
+                <li>
+                  {product.countInStock > 0 && <button onClick={handleAddToCart} className="button primary" >Add to Cart</button>
+                  }
+                </li>
               </ul>
             </div>
           </div>
-          <div className="content-margined">
-            <h2>Reviews</h2>
-            {product.reviews.length === 0 && <div>There is no review.</div>}
-            <ul className="review">
-              {
-                product.reviews.map((review) => (
-                  <li key={review._id}>
-                    <div><b>{review.name}</b></div>
-                    <div className="rating-container">
-                      <Rating value={review.rating} />
-                      <div>
-                        {review.createdAt.substring(0, 10)}
-                      </div>
-                    </div>
-                    <div>
-                      {review.comment}
-                    </div>
-                  </li>
-                ))
-              }
-              <li>
-                <h3>Write a customer reviews</h3>
-
-                {window.isAuth
-                  ? (
-                    <form onSubmit={submitHandler}>
-                      <ul className="form-container">
-                        <li>
-                          <label htmlFor="rating">Rating</label>
-                          <select required value={rating} name="rating" id="rating" onChange={(e) => setRating(e.target.value)}>
-                            <option value="">Select</option>
-                            <option value="1">1 = Poor</option>
-                            <option value="2">2 = Fair</option>
-                            <option value="3">3 = Good</option>
-                            <option value="4">4 = Very Good</option>
-                            <option value="5">5 = Excellent</option>
-                          </select>
-                        </li>
-                        <li>
-                          <label htmlFor="comment">Comment</label>
-
-                          <textarea required value={comment} name="comment" id="comment" onChange={(e) => setComment(e.target.value)} />
-
-                        </li>
-                        <li>
-                          <button type="submit" className="button primary">Submit</button>
-                        </li>
-                      </ul>
-                    </form>
-                  )
-                  : (
-                    <div>
-Please
-                      {' '}
-                      <Link to="/signin">Signin</Link>
-                      {' '}
-to write a review.
-                    </div>
-
-                  ) }
-
-              </li>
-            </ul>
-          </div>
-        </div>
-      )
-  );
+        )
+    }
+   
+  </div>
 }
 export default ProductScreen;
