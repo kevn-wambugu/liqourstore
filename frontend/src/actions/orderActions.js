4f8ec7aac125f8a0d4eeb432d914ea66e77212ab
Kevin Wambugu <wambugunjoroge23@gmail.com>
2020-06-12 18:59:21 +0300

    "Order Screen"

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,92 +1,127 @@
-import Axios from "axios";
+import axios from 'axios';
 import {
   ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL,
-  ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL, MY_ORDER_LIST_REQUEST, MY_ORDER_LIST_SUCCESS, MY_ORDER_LIST_FAIL, ORDER_DELETE_REQUEST, ORDER_DELETE_SUCCESS, ORDER_DELETE_FAIL, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_LIST_FAIL
-} from "../constants/orderConstants";
+  ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL,
+  ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_LIST_FAIL,
+  MY_ORDER_LIST_REQUEST, MY_ORDER_LIST_SUCCESS, MY_ORDER_LIST_FAIL,
+  ORDER_DELETE_REQUEST, ORDER_DELETE_SUCCESS, ORDER_DELETE_FAIL,
+  ORDER_UPDATE_REQUEST, ORDER_UPDATE_SUCCESS, ORDER_UPDATE_FAIL,
+  ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL,
+} from '../constants/orderConstants';
+import { getErrorMessage } from '../util';
 
-const createOrder = (order) => async (dispatch, getState) => {
+const createOrder = (data) => async (dispatch, getState) => {
+  dispatch({ type: ORDER_CREATE_REQUEST });
   try {
-    dispatch({ type: ORDER_CREATE_REQUEST, payload: order });
-    const { userSignin: { userInfo } } = getState();
-    const { data: { data: newOrder } } = await Axios.post("/api/orders", order, {
+    const { userSignin: { userInfo: { token } } } = getState();
+    const { data: { data: order } } = await axios.post('/api/orders', data, {
       headers: {
-        Authorization: ' Bearer ' + userInfo.token
-      }
+        Authorization: `Bearer ${token}`,
+      },
     });
-    dispatch({ type: ORDER_CREATE_SUCCESS, payload: newOrder });
+    dispatch({ type: ORDER_CREATE_SUCCESS, payload: order });
   } catch (error) {
-    dispatch({ type: ORDER_CREATE_FAIL, payload: error.message });
+    dispatch({ type: ORDER_CREATE_FAIL, payload: getErrorMessage(error) });
   }
-}
+};
 
-const listMyOrders = () => async (dispatch, getState) => {
+const updateOrder = (order) => async (dispatch, getState) => {
+  dispatch({ type: ORDER_UPDATE_REQUEST, payload: order });
   try {
-    dispatch({ type: MY_ORDER_LIST_REQUEST });
-    const { userSignin: { userInfo } } = getState();
-    const { data } = await Axios.get("/api/orders/mine", {
-      headers:
-        { Authorization: 'Bearer ' + userInfo.token }
-    });
-    dispatch({ type: MY_ORDER_LIST_SUCCESS, payload: data })
-  } catch (error) {
-    dispatch({ type: MY_ORDER_LIST_FAIL, payload: error.message });
-  }
-}
+    const { userSignin: { userInfo: { token } } } = getState();
 
-const listOrders = () => async (dispatch, getState) => {
-
-  try {
-    dispatch({ type: ORDER_LIST_REQUEST });
-    const { userSignin: { userInfo } } = getState();
-    const { data } = await Axios.get("/api/orders", {
-      headers:
-        { Authorization: 'Bearer ' + userInfo.token }
+    const { data: updatedOrder } = await axios.put(`/api/orders/${order._id}`, order, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     });
-    dispatch({ type: ORDER_LIST_SUCCESS, payload: data })
+    dispatch({ type: ORDER_UPDATE_SUCCESS, payload: updatedOrder });
   } catch (error) {
-    dispatch({ type: ORDER_LIST_FAIL, payload: error.message });
+    dispatch({ type: ORDER_UPDATE_FAIL, payload: getErrorMessage(error) });
   }
-}
-
-const detailsOrder = (orderId) => async (dispatch, getState) => {
-  try {
-    dispatch({ type: ORDER_DETAILS_REQUEST, payload: orderId });
-    const { userSignin: { userInfo } } = getState();
-    const { data } = await Axios.get("/api/orders/" + orderId, {
-      headers:
-        { Authorization: 'Bearer ' + userInfo.token }
-    });
-    dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data })
-  } catch (error) {
-    dispatch({ type: ORDER_DETAILS_FAIL, payload: error.message });
-  }
-}
+};
 
 const payOrder = (order, paymentResult) => async (dispatch, getState) => {
+  dispatch({ type: ORDER_PAY_REQUEST, payload: paymentResult });
   try {
-    dispatch({ type: ORDER_PAY_REQUEST, payload: paymentResult });
-    const { userSignin: { userInfo } } = getState();
-    const { data } = await Axios.put("/api/orders/" + order._id + "/pay", paymentResult, {
-      headers:
-        { Authorization: 'Bearer ' + userInfo.token }
-    });
-    dispatch({ type: ORDER_PAY_SUCCESS, payload: data })
-  } catch (error) {
-    dispatch({ type: ORDER_PAY_FAIL, payload: error.message });
-  }
-}
+    const { userSignin: { userInfo: { token } } } = getState();
 
-const deleteOrder = (orderId) => async (dispatch, getState) => {
-  try {
-    dispatch({ type: ORDER_DELETE_REQUEST, payload: orderId });
-    const { userSignin: { userInfo } } = getState();
-    const { data } = await Axios.delete("/api/orders/" + orderId, {
-      headers:
-        { Authorization: 'Bearer ' + userInfo.token }
+    const { data: paidOrder } = await axios.put(`/api/orders/${order._id}/pay`, paymentResult, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     });
-    dispatch({ type: ORDER_DELETE_SUCCESS, payload: data })
+    dispatch({ type: ORDER_PAY_SUCCESS, payload: paidOrder });
   } catch (error) {
-    dispatch({ type: ORDER_DELETE_FAIL, payload: error.message });
+    dispatch({ type: ORDER_PAY_FAIL, payload: getErrorMessage(error) });
   }
-}
-export { createOrder, detailsOrder, payOrder, listMyOrders, listOrders, deleteOrder };
+};
+
+const deliverOrder = (order) => async (dispatch, getState) => {
+  dispatch({ type: ORDER_PAY_REQUEST, payload: {} });
+  try {
+    const { userSignin: { userInfo: { token } } } = getState();
+
+    const { data: paidOrder } = await axios.put(`/api/orders/${order._id}/deliver`, {}, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    dispatch({ type: ORDER_PAY_SUCCESS, payload: paidOrder });
+  } catch (error) {
+    dispatch({ type: ORDER_PAY_FAIL, payload: getErrorMessage(error) });
+  }
+};
+
+const deleteOrder = (order) => async (dispatch, getState) => {
+  dispatch({ type: ORDER_DELETE_REQUEST, payload: order });
+  try {
+    const { userSignin: { userInfo: { token } } } = getState();
+    const { data: deletedOrder } = await axios.delete(`/api/orders/${order._id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    dispatch({ type: ORDER_DELETE_SUCCESS, payload: deletedOrder });
+  } catch (error) {
+    dispatch({ type: ORDER_DELETE_FAIL, payload: getErrorMessage(error) });
+  }
+};
+
+
+const detailsOrder = (orderId) => async (dispatch) => {
+  try {
+    dispatch({ type: ORDER_DETAILS_REQUEST, payload: orderId });
+    const result = await axios(`/api/orders/${orderId}`);
+    dispatch({ type: ORDER_DETAILS_SUCCESS, payload: result.data });
+  } catch (error) {
+    dispatch({ type: ORDER_DETAILS_FAIL, payload: getErrorMessage(error) });
+  }
+};
+
+const listOrders = (category) => async (dispatch, getState) => {
+  dispatch({ type: ORDER_LIST_REQUEST, payload: category });
+  try {
+    const { userSignin: { userInfo: { token } } } = getState();
+    const result = await axios('/api/orders', { headers: { Authorization: `Bearer ${token}` } });
+    dispatch({ type: ORDER_LIST_SUCCESS, payload: result.data });
+  } catch (error) {
+    dispatch({ type: ORDER_LIST_FAIL, payload: getErrorMessage(error) });
+  }
+};
+
+const listMyOrders = () => async (dispatch, getState) => {
+  dispatch({ type: MY_ORDER_LIST_REQUEST });
+  try {
+    const { userSignin: { userInfo: { token } } } = getState();
+    const result = await axios('/api/orders/mine', { headers: { Authorization: `Bearer ${token}` } });
+    dispatch({ type: MY_ORDER_LIST_SUCCESS, payload: result.data });
+  } catch (error) {
+    dispatch({ type: MY_ORDER_LIST_FAIL, payload: getErrorMessage(error) });
+  }
+};
+
+export {
+  createOrder, detailsOrder, listOrders, listMyOrders,
+  updateOrder, deleteOrder, payOrder, deliverOrder,
+};
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,128 +1,181 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { createOrder, detailsOrder, payOrder } from '../actions/orderActions';
+import LoadingBox from '../components/LoadingBox';
+import ErrorBox from '../components/ErrorBox';
+import { detailsOrder, payOrder, deliverOrder } from '../actions/orderActions';
 import PaypalButton from '../components/PaypalButton';
-function OrderScreen(props) {
+import { ORDER_PAY_RESET } from '../constants/orderConstants';
 
-  const orderPay = useSelector(state => state.orderPay);
-  const { loading: loadingPay, success: successPay, error: errorPay } = orderPay;
+function OrderScreen(props) {
   const dispatch = useDispatch();
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { loading, error, order } = orderDetails;
+  const orderPay = useSelector((state) => state.orderPay);
+  const { loading: loadingPay, error: errorPay, success: successPay } = orderPay;
+  const orderDeliver = useSelector((state) => state.orderDeliver);
+  const { loading: loadingDeliver, error: errorDeliver, success: successDeliver } = orderDeliver || {};
+
+  const redirect = props.location.search ? props.location.search.split('=')[1] : '/profile';
+
   useEffect(() => {
     if (successPay) {
-      props.history.push("/profile");
+      dispatch({ type: ORDER_PAY_RESET });
+      props.history.push('/profile');
     } else {
       dispatch(detailsOrder(props.match.params.id));
     }
     return () => {
+      //
     };
-  }, [successPay]);
-
+  }, [dispatch, successPay]);
   const handleSuccessPayment = (paymentResult) => {
     dispatch(payOrder(order, paymentResult));
-  }
-
-  const orderDetails = useSelector(state => state.orderDetails);
-  const { loading, order, error } = orderDetails;
-
-  return loading ? <div>Loading ...</div> : error ? <div>{error}</div> :
-
-    <div>
-      <div className="placeorder">
-        <div className="placeorder-info">
-          <div>
+  };
+  const handleDeliverOrder = () => {
+    dispatch(deliverOrder(order));
+  };
+  return (
+    loading ? <LoadingBox /> : error ? <ErrorBox message={error} />
+      : (
+        <div>
+          <div className="back-to-results">
+            <Link to={redirect}> ‹ Back to list</Link>
+            <br />
             <h3>
-              Shipping
-          </h3>
-            <div>
-              {order.shipping.mobnumber}, {order.shipping.city},
-          {order.shipping.postalCode}, {order.shipping.country},
+              Order
+              {' '}
+              {order._id}
+            </h3>
           </div>
-            <div>
-              {order.isDelivered ? "Delivered at " + order.deliveredAt : "Not Delivered."}
-            </div>
-          </div>
-          <div>
-            <h3>Payment</h3>
-            <div>
-              Payment Method: {order.payment.paymentMethod}
-            </div>
-            <div>
-              {order.isPaid ? "Paid at " + order.paidAt : "Not Paid."}
-            </div>
-          </div>
-          <div>
-            <ul className="cart-list-container">
-              <li>
-                <h3>
-                  Shopping Cart
-          </h3>
+
+          <div className="placeorder">
+
+            <div className="placeorder-info">
+              <div>
+                <h3>Shipping Address</h3>
                 <div>
-                  Price
-          </div>
-              </li>
-              {
-                order.orderItems.length === 0 ?
-                  <div>
-                    Cart is empty
-          </div>
-                  :
-                  order.orderItems.map(item =>
+                  {order.shipping.mobnumber}
+                  ,
+                  {' '}
+                  {order.shipping.city}
+                  ,
+                  {' '}
+                  {order.shipping.country}
+                  ,
+                  {' '}
+                  {order.shipping.postalCode}
+                </div>
+                <h3>
+                  {order.isDelivered ? `Delivered At ${order.deliveredAt}` : 'Not Delivered'}
+
+                </h3>
+              </div>
+              <div>
+                <h3>Payment Method</h3>
+                <div>
+                  {order.payment.paymentMethod}
+                </div>
+                <h3>
+
+                  {order.isPaid ? `Paid At ${order.paidAt}` : 'Not Paid'}
+                </h3>
+              </div>
+              <div>
+
+                <ul className="cart-list-container">
+                  <li>
+                    <h3>Order Items</h3>
+                    <div>Price</div>
+                  </li>
+                  {order.orderItems.map((item) => (
                     <li key={item._id}>
                       <div className="cart-image">
                         <img src={item.image} alt="product" />
                       </div>
                       <div className="cart-name">
                         <div>
-                          <Link to={"/product/" + item.product}>
-                            {item.name}
-                          </Link>
+                          <Link to={`/product/${item._id}`}>{item.name}</Link>
+                        </div>
+                        <div className="cart-list-actions">
+                          Qty:
+                          {' '}
+                          {item.qty}
 
                         </div>
-                        <div>
-                          Qty: {item.qty}
-                        </div>
                       </div>
-                      <div className="cart-price">
-                        KSH{item.price}
+                      <div className="order-price">
+                        KSH
+                        {item.price}
                       </div>
                     </li>
-                  )
-              }
-            </ul>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="placeorder-actions">
+
+              <ul>
+                {userInfo.isAdmin  && !order.isDelivered && (
+                  <li>
+
+                    <button onClick={handleDeliverOrder} type="button" className="button primary">
+                      Deliver Order
+                    </button>
+
+                  </li>
+                )}
+
+                {!order.isPaid
+                  && (
+                    <li>
+                      <PaypalButton
+                        amount="1.00"
+                        onError={() => console.log('error')}
+                        onSuccess={handleSuccessPayment}
+                        onCancel={() => console.log('cancel')}
+                      />
+                    </li>
+                  )}
+
+                <li>
+                  <h3>Order Summary</h3>
+                </li>
+                <li>
+                  <div>Items:</div>
+                  <div>
+                    KSH
+                    {order.itemPrice}
+                  </div>
+                </li>
+                <li>
+                  <div>Shipping:</div>
+                  <div>{order.shippingPrice ? `$${order.shippingPrice}` : 'Free'}</div>
+                </li>
+                <li>
+                  <div>Tax:</div>
+                  <div>
+                    KSH
+                    {order.taxPrice}
+                  </div>
+                </li>
+                <li>
+                  <div>Order Total:</div>
+                  <div>
+                    KSH
+                    {order.totalPrice}
+
+                  </div>
+                </li>
+              </ul>
+
+
+            </div>
           </div>
         </div>
-        <div className="placeorder-action">
-          <ul>
-            <li className="placeorder-actions-payment">
-              {loadingPay && <div>Finishing Payment...</div>}
-              {!order.isPaid &&
-                <PaypalButton
-                  amount={order.totalPrice}
-                  onSuccess={handleSuccessPayment} />
-              }
-            </li>
-            <li>
-              <h3>Order Summary</h3>
-            </li>
-            <li>
-              <div>Items</div>
-              <div>KSH{order.itemsPrice}</div>
-            </li>
-            <li>
-              <div>Delivery fee</div>
-              <div>KSH{order.shippingPrice}</div>
-            </li>
-
-            <li>
-              <div>Order Total</div>
-              <div>KSH{order.totalPrice}</div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
+      )
+  );
 }
-
 export default OrderScreen;
