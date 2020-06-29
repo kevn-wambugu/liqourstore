e2686c93e91490ab69d4bf7e2271509f117ad9fc
Kevin Wambugu <wambugunjoroge23@gmail.com>
2020-06-29 08:59:56 +0300

    'commit'

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,25 +8,24 @@ function HomeScreen(props) {
   const [searchKeyword, setSearchKeyword] = useState('');
   const [sortOrder, setSortOrder] = useState('');
   const category = props.match.params.id ? props.match.params.id : '';
-  const brand = props.match.params.id ? props.match.params.id : '';
   const productList = useSelector(state => state.productList);
   const { products, loading, error } = productList;
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(listProducts(category || brand));
+    dispatch(listProducts(category));
 
     return () => {
       //
     };
-  }, [category] || [brand]);
+  }, [category]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(listProducts(brand,category, searchKeyword, sortOrder))
+    dispatch(listProducts(category, searchKeyword, sortOrder))
   }
   const sortHandler = (e) => {
     setSortOrder(e.target.value);
-    dispatch(listProducts(brand,category, searchKeyword, sortOrder))
+    dispatch(listProducts(category, searchKeyword, sortOrder))
   }
 
   return <>
@@ -42,7 +41,7 @@ function HomeScreen(props) {
         </form>
       </li>
       <li>
-        Sort By {' '}
+        Sort By{' '}
         <select name="sortOrder" onChange={sortHandler}>
           <option value="">Newest</option>
           <option value="lowest">Lowest</option>
